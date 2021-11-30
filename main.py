8f4803003a835f5429697edeb29f9ac3d6f0e471
Courtlin Holt-Nguyen <courtlinholt@gmail.com>
2021-11-30 06:16:20 +0000

    Imported pandas, added data to web app

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
 import streamlit as st
+import pandas
+
+data = {
+  'Series 1':[1,3,4,5,7],
+  'Series 2':[10,30,40,100,250]
+
+}
+
+df = pandas.DataFrame(data)
 
 st.title('Our First Streamlit App')
 st.subheader('Introducing Streamlit in Automate Everything with Python')
-st.write('''This is our first web app
+st.write('''This is our first web app.
 Enjoy it!
 ''')
+
+st.write(df)
